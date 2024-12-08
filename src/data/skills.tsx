09f556823775f3c9e237b44dc2b9e28674d0fd1e
pegasus <aniscima@gmail.com>
2024-12-08 22:04:00 +0530

    Add ts

--- src/data/skills.tsx
+++ src/data/skills.tsx
@@ -73,6 +73,23 @@ export const skillCategories = [
           </a>
         ),
       },
+      {
+        name: 'TypeScript',
+        icon: (
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/Typescript"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/typescript-colored.svg"
+              width="36"
+              height="36"
+              alt="Typescript"
+            />
+          </a>
+        ),
+      },
     ],
   },
   {
